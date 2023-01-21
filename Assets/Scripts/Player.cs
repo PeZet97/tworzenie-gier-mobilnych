3eeb37cffe570bcde6f590b3a7dd7239a3ffa033
Piotr Zabrocki <118100977+PeZet97@users.noreply.github.com>
2023-01-21 19:07:15 +0100

    Add files via upload
    21.01.2023 - Dodanie możliwości strzelania i prefabu Bullet, który niszczy się po zetknięciu z boundary levelu.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     public float thrustSpeed = 1.0f;
     public float turnSpeed = 1.0f;
 
@@ -25,6 +26,10 @@ public class Player : MonoBehaviour
         } else {
             _turnDirection = 0.0f;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
     }
 
     private void FixedUpdate()
@@ -36,5 +41,11 @@ public class Player : MonoBehaviour
         if (_turnDirection != 0.0f) {
             _rigidbody.AddTorque(_turnDirection * this.turnSpeed);
         }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
     }
 }
