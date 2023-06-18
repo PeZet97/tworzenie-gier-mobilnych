3eef3ab4bd9911f8451576e3638e427a737c7eea
pola <94833816+sunyflower@users.noreply.github.com>
2023-06-18 19:08:29 +0200

    Score ale Lifes UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,24 @@
 //Śledzi aktualny stan gry: punkty, życia, stan obiektu Gracz (czy żyje, czy nie)
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Player player;
     public ParticleSystem explosion;
-    public float respawnTime = 3.0f; 
+    public float respawnTime = 3.0f;
     public float respawnInvulnerabilityTime = 3.0f;
     public int lives = 3;
     public int score = 0;
+
+    public Text scoreText;
+    public Text livesText;
+
+    private void Start()
+    {
+        SetScore(0);
+        SetLives(3);
+    }
 
     public void AsteroidDestroyed(Asteroid asteroid)
     {
@@ -17,15 +27,15 @@ public class GameManager : MonoBehaviour
 
         if (asteroid.size < 0.6f)
         {
-            this.score += 50; // punkty za male asteroidy
+            SetScore(score += 50); // punkty za male asteroidy
         }
         else if (asteroid.size < 1.2f)
         {
-            this.score += 25; // punkty za srednie asteroidy
+            SetScore(score += 25);  // punkty za srednie asteroidy
         }
         else
         {
-            this.score += 10; // punkty za male asteroidy
+            SetScore(score += 10); // punkty za male asteroidy
         }
     }
 
@@ -34,13 +44,16 @@ public class GameManager : MonoBehaviour
         this.explosion.transform.position = this.player.transform.position;
         this.explosion.Play();
 
-        this.lives--;
+        SetLives(lives - 1);
 
-        if (this.lives <= 0) { //jeśli liczba lives jest równa 0, następuje Game Over.
+        if (this.lives <= 0)
+        { //jeśli liczba lives jest równa 0, następuje Game Over.
             GameOver();
-        } else {
-            Invoke (nameof(Respawn), this.respawnTime);  // jeśli nie jest równa 0, następuje respawn.
-        } 
+        }
+        else
+        {
+            Invoke(nameof(Respawn), this.respawnTime);  // jeśli nie jest równa 0, następuje respawn.
+        }
     }
 
     private void Respawn()
@@ -48,7 +61,7 @@ public class GameManager : MonoBehaviour
         this.player.transform.position = Vector3.zero; //Obiekt Player respawnuje się w centrum sceny.
         this.player.gameObject.layer = LayerMask.NameToLayer("Ignore Collisions"); //Po respawnie Playera, zmienia się jego layer
         this.player.gameObject.SetActive(true); //Obiek Player respawnuje się, status gameObject zmienia się na true.
-       Invoke(nameof(TurnOnCollisions), this.respawnInvulnerabilityTime);
+        Invoke(nameof(TurnOnCollisions), this.respawnInvulnerabilityTime);
     }
 
     private void TurnOnCollisions()
@@ -58,7 +71,19 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
-       
+
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        livesText.text = lives.ToString();
     }
 
 }
